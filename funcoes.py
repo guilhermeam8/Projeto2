0771b02a5a385ac0a8874b9034e144c6ba24bd1d
Henrique Montemor <henriquemm7@al.insper.edu.br>
2025-05-06 12:03:11 -0300

    funcao calcula_pontos_sequencia

--- funcoes.py
+++ funcoes.py
@@ -31,3 +31,13 @@ def calcula_pontos_soma (lista):
     for numeros in lista:
         resposta += numeros
     return resposta
+def calcula_pontos_sequencia_baixa(faces):
+    sequencias = [[1, 2, 3, 4],[2, 3, 4, 5],[3, 4, 5, 6]]
+    for sequencia in sequencias:
+        contador = 0
+        for numero in sequencia:
+            if numero in faces:
+                contador += 1
+        if contador == 4:
+            return 15
+    return 0
